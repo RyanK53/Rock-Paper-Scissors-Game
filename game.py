b1baf3012664ccd81a7e1aed8dc38675ad1beb3a
RyanK53 <rmk101@georgetown.edu>
2021-09-21 11:46:20 -0400

    Update game.py

--- game.py
+++ game.py
@@ -19,9 +19,8 @@ computer_choice = random.choice(options)
 print("Computer Chose:") 
 print(computer_choice)
 
-if (user_choice != 'Rock' or 'Paper' or 'Scissors'):
-    print("Incorrect")
-elif(user_choice == computer_choice):
+
+if(user_choice == computer_choice):
     print("Draw, try again!")
 elif(user_choice == 'Rock' and computer_choice == 'Scissors'):
     print("You win! Nice Job")
@@ -35,3 +34,5 @@ elif(user_choice == 'Rock' and computer_choice == 'Paper'):
     print("You lose :( try again next time!")
 elif(user_choice == 'Paper' and computer_choice == 'Rock'):
     print("You win! Nice Job")
+elif (user_choice != 'Rock' or 'Paper' or 'Scissors'):
+    print("Incorrect")
