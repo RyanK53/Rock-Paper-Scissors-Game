71ccd07fd1c73a9997d7794d225e95786389107e
RyanK53 <rmk101@georgetown.edu>
2021-09-21 11:15:02 -0400

    Update game.py

--- game.py
+++ game.py
@@ -20,3 +20,15 @@ print(computer_choice)
 
 if(user_choice == computer_choice):
     print("Draw, try again!")
+elif(user_choice == 'Rock' and computer_choice == 'Scissors'):
+    print("You win! Nice Job")
+elif(user_choice == 'Paper' and computer_choice == 'Scissors'):
+    print("You lose :( try again next time!")
+elif(user_choice == 'Scissors' and computer_choice == 'Rock'):
+    print("You lose :( try again next time!")
+elif(user_choice == 'Scissors' and computer_choice == 'Paper'):
+    print("You win! Nice Job")
+elif(user_choice == 'Rock' and computer_choice == 'Paper'):
+    print("You lose :( try again next time!")
+elif(user_choice == 'Paper' and computer_choice == 'Rock'):
+    print("You win! Nice Job")
