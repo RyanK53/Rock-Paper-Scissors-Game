f2dd950527e9177f0928c5729f2c54a9ac752129
RyanK53 <rmk101@georgetown.edu>
2021-09-26 18:33:41 -0400

    Update game.py

--- game.py
+++ game.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-PLAYER_NAME = os.getenv("PLAYER_NAME")
-print("Welcome", PLAYER_NAME,"to my Rock-Paper-Scissors game...")
+PLAYER_NAME = os.getenv("USER_NAME")
+print("Welcome", USER_NAME,"to my Rock-Paper-Scissors game...")
 print("----------------")
 print("Welcome let's play Rock, Paper, Scissors")
 print("----------------")
